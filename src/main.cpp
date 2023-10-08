d6c41907991a508ad12f915c284d52c5d2aa3dff
Gabriel de Souza <devgabrieldesouza@gmail.com>
2023-10-08 16:50:09 -0300

    Vulkan instance

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vulkan/vulkan.h>
+
+int main(){
+	VkApplicationInfo appInfo = {};
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pApplicationName = "Hello Vulkan";
+	appInfo.pEngineName = "Ponggine";
+
+	VkInstanceCreateInfo instanceInfo = {};
+	instanceInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	instanceInfo.pApplicationInfo = &appInfo;
+
+	VkInstance instance;
+
+	VkResult result = vkCreateInstance(&instanceInfo, 0, &instance);
+	
+	if(result == VK_SUCCESS) {
+		std::cout << "Successfully created Vulkan instance" << std::endl;
+	}
+
+	return 0;
+}
